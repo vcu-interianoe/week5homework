a419fbec3beb5d3320d67386e73721efa0583dfe
eddy <interianoe@vcu.edu>
2021-03-08 19:46:33 -0500

    Worked on it more

--- fun/homework.py
+++ fun/homework.py
@@ -6,9 +6,11 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
+    if incoming_list is None:
+        return 0
     largest_value = max(incoming_list)
     return largest_value
-    pass
+
 
 
 def find_least_number(incoming_list):
@@ -16,9 +18,12 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
+
+    if incoming_list is None:
+        return 0
     smallest_value = min(incoming_list)
     return smallest_value
-    pass
+    
 
 
 def add_list_numbers(incoming_list):
@@ -26,9 +31,11 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
+
+    if incoming_list is None:
+        return 0
     total_value = sum(incoming_list)
     return total_value
-    pass
 
 
 def longest_value_key(incoming_dict):
@@ -36,6 +43,10 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    final_key = max(incoming_dict, key=lambda k: len(incoming_dict[k]))
-    return final_key
-    pass
+    if incoming_dict is not None:
+        if len(incoming_dict) == 0:
+            return None
+        dict_key = max(incoming_dict, key=lambda k: len(incoming_dict[k]))
+        return dict_key
+    return None
+    
